1aa1ac362e304055d5f852ed40872e6023bcaa60
Yash Singh <yashs33244@gmail.com>
2024-06-19 20:01:00 +0530

    new edits

--- apps/user-app/components/SidebarItem.tsx
+++ apps/user-app/components/SidebarItem.tsx
@@ -16,6 +16,11 @@ export const SidebarItem = ({ href, title, icon }: { href: string; title: string
         <div className={`font-bold ${selected ? "text-[#6a51a6]" : "text-slate-500"}`}>
             {title}
         </div>
+
+
+
+
+        
     </div>
 
 
